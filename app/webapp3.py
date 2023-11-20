d60038014d320d1c59ef1e2f4ed7c4364429e195
fayssalsb123 <147110202+fayssalsb123@users.noreply.github.com>
2023-11-20 10:33:20 +0100

    Update webapp3.py

--- app/webapp3.py
+++ app/webapp3.py
@@ -121,9 +121,9 @@ def add_predictions(input_data):
     st.subheader("Cell cluster prediction ")
     st.write("the cell cluster is:")
     if prediction[0]==0:
-        st.write("Benign")
+        st.write("Benign 😄")
     else:
-        st.write("malignant")
+        st.write("malignant 😢")
     
     st.write("Probability of being benign:",model.predict_proba(scaled_input_array)[0][0])
     st.write("Probability of being malignant:",model.predict_proba(scaled_input_array)[0][1])
